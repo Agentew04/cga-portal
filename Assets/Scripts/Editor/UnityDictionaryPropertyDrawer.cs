29b700ce763ea319a452a412216b1e72b0365b62
rappelt <rappelt@inf.ufsm.br>
2024-11-13 14:06:56 -0300

    tentativa drawer dict

--- Assets/Scripts/Editor/UnityDictionaryPropertyDrawer.cs
+++ Assets/Scripts/Editor/UnityDictionaryPropertyDrawer.cs
@@ -3,77 +3,141 @@ using UnityEngine;
 using UnityEditor;
 using System.Reflection;
 using PortalGame;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System.Collections.Generic;
 
 /// <summary>
 /// <see cref="PropertyDrawer"/> que mostra no inspetor
 /// um tipo de dicionario customizado.
 /// </summary>
-[CustomPropertyDrawer(typeof(UnityDictionary<,>))]
-
+[CustomPropertyDrawer(typeof(UnityDictionary<,>), true)]
 public class UnityDictionaryPropertyDrawer : PropertyDrawer {
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        EditorGUI.BeginProperty(position, label, property);
+    private VisualElement container;
 
-        // Find the private field m_Dictionary using reflection
-        var targetObject = fieldInfo.GetValue(property.serializedObject.targetObject);
-        var dictionaryField = targetObject.GetType().GetField("m_Dictionary", BindingFlags.NonPublic | BindingFlags.Instance);
+    private ListView itemsListView;
+    private Toggle allowDuplicatesToggle;
 
-        // Draw the foldout
-        property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), property.isExpanded, label);
+    public override VisualElement CreatePropertyGUI(SerializedProperty property) {
+        // cria a arvore
+        var vsTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/UnityDictionaryTree.uxml");
+        container = vsTree.CloneTree(property.propertyPath);
 
-        if (property.isExpanded && dictionaryField.GetValue(targetObject) is System.Collections.IList dictionaryList) {
-            EditorGUI.indentLevel++;
+        // binda a checkbox
+        allowDuplicatesToggle = container.Q<Toggle>("allowDuplicatesToggle");
+        var duplicatesProperty = property.FindPropertyRelative("m_duplicatesAllowed");
+        allowDuplicatesToggle.BindProperty(duplicatesProperty);
 
-            // Iterate over each entry in the dictionary
-            for (int i = 0; i < dictionaryList.Count; i++) {
-                var element = dictionaryList[i];
+        // setar a list view
+        itemsListView = container.Q<ListView>("itemsListView");
+        var dictProperty = property.FindPropertyRelative("m_Dictionary");
 
-                // Use reflection to get Key and Value fields
-                var keyField = element.GetType().GetProperty("Key");
-                var valueField = element.GetType().GetProperty("Value");
+        // inicializa a listview com os itens do dicionario
+        itemsListView.itemsSource = GetKeyValuePairs(dictProperty);
+        itemsListView.makeItem = () => new Label();
+        itemsListView.bindItem = (element, i) => {
+            var itemProperty = dictProperty.GetArrayElementAtIndex(i);
+            var keyProperty = itemProperty.FindPropertyRelative("Key");
+            var valueProperty = itemProperty.FindPropertyRelative("Value");
 
-                var key = keyField.GetValue(element);
-                var value = valueField.GetValue(element);
+            // Display key-value pair in a flexible way based on type
+            (element as Label).text = $"{GetPropertyDisplayName(keyProperty)} : {GetPropertyDisplayName(valueProperty)}";
+        };
 
-                var keyRect = new Rect(position.x, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.35f, EditorGUIUtility.singleLineHeight);
-                var valueRect = new Rect(position.x + position.width * 0.4f, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.45f, EditorGUIUtility.singleLineHeight);
-                var deleteButtonRect = new Rect(position.x + position.width * 0.9f, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.1f, EditorGUIUtility.singleLineHeight);
+        itemsListView.
 
-                // Display Key and Value using appropriate fields
-                var newKey = EditorGUI.EnumPopup(keyRect, (Enum)key);
-                var valueType = value?.GetType() ?? typeof(UnityEngine.Object); // Use Object if value is null
-                var newValue = EditorGUI.ObjectField(valueRect, (UnityEngine.Object)value, valueType, true);
-                // Set the new key and value
-                keyField.SetValue(element, newKey);
-                valueField.SetValue(element, newValue);
 
-                if (GUI.Button(deleteButtonRect, "X")) {
-                    dictionaryList.RemoveAt(i);
-                    break; // Exit loop to avoid modifying list while iterating
-                }
-            }
-
-            // Add button to add new dictionary entry
-            if (GUI.Button(new Rect(position.x, position.y + (dictionaryList.Count + 1) * EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight), "Add Entry")) {
-                var keyValuePairType = dictionaryField.FieldType.GenericTypeArguments[0]; // Get KeyValuePair type
-                var newElement = Activator.CreateInstance(keyValuePairType);
-                dictionaryList.Add(newElement);
-            }
-
-            EditorGUI.indentLevel--;
-        }
-
-        EditorGUI.EndProperty();
+        return container;
     }
 
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        var targetObject = fieldInfo.GetValue(property.serializedObject.targetObject);
-        var dictionaryField = targetObject.GetType().GetField("m_Dictionary", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        if (property.isExpanded && dictionaryField.GetValue(targetObject) is System.Collections.IList dictionaryList) {
-            return (dictionaryList.Count + 2) * EditorGUIUtility.singleLineHeight;
+    private List<SerializedProperty> GetKeyValuePairs(SerializedProperty dictProperty) {
+        List<SerializedProperty> keyValuePairs = new();
+        for (int i = 0; i < dictProperty.arraySize; i++) {
+            keyValuePairs.Add(dictProperty.GetArrayElementAtIndex(i));
         }
-        return EditorGUIUtility.singleLineHeight;
+        return keyValuePairs;
     }
+
+    private string GetPropertyDisplayName(SerializedProperty property) {
+        switch (property.propertyType) {
+            case SerializedPropertyType.Enum:
+                return property.enumDisplayNames[property.enumValueIndex];
+            case SerializedPropertyType.Integer:
+                return property.intValue.ToString();
+            case SerializedPropertyType.Float:
+                return property.floatValue.ToString();
+            case SerializedPropertyType.String:
+                return property.stringValue;
+            case SerializedPropertyType.ObjectReference:
+                return property.objectReferenceValue ? property.objectReferenceValue.name : "None";
+            default:
+                return "Unsupported Type";
+        }
+    }
+
+    //public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+    //    EditorGUI.BeginProperty(position, label, property);
+
+    //    // Find the private field m_Dictionary using reflection
+    //    var targetObject = fieldInfo.GetValue(property.serializedObject.targetObject);
+    //    var dictionaryField = targetObject.GetType().GetField("m_Dictionary", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    //    // Draw the foldout
+    //    property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), property.isExpanded, label);
+
+    //    if (property.isExpanded && dictionaryField.GetValue(targetObject) is System.Collections.IList dictionaryList) {
+    //        EditorGUI.indentLevel++;
+
+    //        // Iterate over each entry in the dictionary
+    //        for (int i = 0; i < dictionaryList.Count; i++) {
+    //            var element = dictionaryList[i];
+
+    //            // Use reflection to get Key and Value fields
+    //            var keyField = element.GetType().GetProperty("Key");
+    //            var valueField = element.GetType().GetProperty("Value");
+
+    //            var key = keyField.GetValue(element);
+    //            var value = valueField.GetValue(element);
+
+    //            var keyRect = new Rect(position.x, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.35f, EditorGUIUtility.singleLineHeight);
+    //            var valueRect = new Rect(position.x + position.width * 0.4f, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.45f, EditorGUIUtility.singleLineHeight);
+    //            var deleteButtonRect = new Rect(position.x + position.width * 0.9f, position.y + (i + 1) * EditorGUIUtility.singleLineHeight, position.width * 0.1f, EditorGUIUtility.singleLineHeight);
+
+    //            // Display Key and Value using appropriate fields
+    //            var newKey = EditorGUI.EnumPopup(keyRect, (Enum)key);
+    //            var valueType = value?.GetType() ?? typeof(UnityEngine.Object); // Use Object if value is null
+    //            var newValue = EditorGUI.ObjectField(valueRect, (UnityEngine.Object)value, valueType, true);
+    //            // Set the new key and value
+    //            keyField.SetValue(element, newKey);
+    //            valueField.SetValue(element, newValue);
+
+    //            if (GUI.Button(deleteButtonRect, "X")) {
+    //                dictionaryList.RemoveAt(i);
+    //                break; // Exit loop to avoid modifying list while iterating
+    //            }
+    //        }
+
+    //        // Add button to add new dictionary entry
+    //        if (GUI.Button(new Rect(position.x, position.y + (dictionaryList.Count + 1) * EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight), "Add Entry")) {
+    //            var keyValuePairType = dictionaryField.FieldType.GenericTypeArguments[0]; // Get KeyValuePair type
+    //            var newElement = Activator.CreateInstance(keyValuePairType);
+    //            dictionaryList.Add(newElement);
+    //        }
+
+    //        EditorGUI.indentLevel--;
+    //    }
+
+    //    EditorGUI.EndProperty();
+    //}
+
+    //public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+    //    var targetObject = fieldInfo.GetValue(property.serializedObject.targetObject);
+    //    var dictionaryField = targetObject.GetType().GetField("m_Dictionary", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    //    if (property.isExpanded && dictionaryField.GetValue(targetObject) is System.Collections.IList dictionaryList) {
+    //        return (dictionaryList.Count + 2) * EditorGUIUtility.singleLineHeight;
+    //    }
+    //    return EditorGUIUtility.singleLineHeight;
+    //}
 }
--- Assets/Scripts/UnityDictionary.cs
+++ Assets/Scripts/UnityDictionary.cs
@@ -1,9 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace PortalGame {
+    /// <summary>
+    /// A kind of dictionary that works within the unity editor
+    /// and is serializable.
+    /// </summary>
+    /// <typeparam name="K">The type of the Key</typeparam>
+    /// <typeparam name="V">The type of the value</typeparam>
     [Serializable]
     public class UnityDictionary<K, V> : IEnumerable<UnityDictionary<K, V>.KeyValuePair>
                                         where K : Enum {
@@ -17,16 +24,42 @@ namespace PortalGame {
             public V Value { get; set; }
         }
 
+        [SerializeField, Tooltip("Defines if this dictionary allows duplicate keys")]
+        private bool m_duplicatesAllowed = true;
+
+        /// <summary>
+        /// Defines if this dictionary allows duplicate keys.
+        /// </summary>
+        public bool DuplicatesAllowed {
+            get => m_duplicatesAllowed;
+            set => m_duplicatesAllowed = value;
+        }
+
+        public ICollection<K> Keys => throw new NotImplementedException();
+
+        public ICollection<V> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
         [SerializeField]
         private List<KeyValuePair> m_Dictionary = new();
 
+        /// <summary>
+        /// Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <remarks>
+        /// Ignores <see cref="DuplicatesAllowed"/> property, always overwrites
+        /// the first entry with the same key on set. Always gets the first entry.
+        /// </remarks>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException">Thrown when the key is not present</exception>
         public V this[K key] {
             get {
                 var kvp = m_Dictionary.Find(x => x.Key.Equals(key));
-                if (kvp == null) { 
-                    throw new KeyNotFoundException();
-                }
-                return kvp.Value;
+                return kvp == null ? throw new KeyNotFoundException() : kvp.Value;
             }
             set {
                 var kvp = m_Dictionary.Find(x => x.Key.Equals(key));
@@ -38,6 +71,7 @@ namespace PortalGame {
             }
         }
 
+
         public IEnumerator<KeyValuePair> GetEnumerator() {
             return m_Dictionary.GetEnumerator();
         }
@@ -45,5 +79,60 @@ namespace PortalGame {
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
             return GetEnumerator();
         }
+
+        public void Add(K key, V value) {
+            if (!m_duplicatesAllowed && ContainsKey(key)) {
+                throw new DuplicateKeyException();
+            }
+
+            m_Dictionary.Add(new KeyValuePair { Key = key, Value = value });
+        }
+
+        public void Add(KeyValuePair kvp) {
+            Add(kvp.Key, kvp.Value);
+        }
+
+        public bool ContainsKey(K key) {
+            return m_Dictionary.Exists(x => x.Key.Equals(key));
+        }
+
+        public bool Remove(K key) {
+            return m_Dictionary.RemoveAll(x => x.Key.Equals(key)) > 0;
+        }
+
+        public bool Remove(KeyValuePair kvp) {
+            return m_Dictionary.RemoveAll(x => x.Key.Equals(kvp.Key) && x.Value.Equals(kvp.Value)) > 0;
+        }
+
+        public bool TryGetValue(K key, out V value) {
+            var kvp = m_Dictionary.Find(x => x.Key.Equals(key));
+            if (kvp == null) {
+                value = default;
+                return false;
+            }
+
+            value = kvp.Value;
+            return true;
+        }
+
+        public void Clear() {
+            m_Dictionary.Clear();
+        }
+
+        public bool Contains(KeyValuePair item) {
+            return m_Dictionary.Exists(x => x.Key.Equals(item.Key) && x.Value.Equals(item.Value));
+        }
+
+        public bool Contains(Predicate<KeyValuePair> match) {
+            return m_Dictionary.Exists(match);
+        }
+    }
+
+    [Serializable]
+    public class DuplicateKeyException : Exception {
+        public DuplicateKeyException() { }
+        public DuplicateKeyException(string message) : base(message) { }
+        public DuplicateKeyException(string message, System.Exception inner) : base(message, inner) { }
+        protected DuplicateKeyException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
